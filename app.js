f7554c6616b6d693c8f918e59b5daff0be2392fe
Dominic Shelton <drs73@uclive.ac.nz>
2015-12-05 01:32:40 +1300

    Changed response display style
    Responses are now attachments, removed flavour, illustrator and pack.
    Added faction colours.

--- app.js
+++ app.js
@@ -14,14 +14,17 @@ var messages = {
     NO_RESULTS: 'You successfully access R&D but it doesn\'t hold what you\'re looking for',
     TOO_MANY: ' results!? you tryna overflow my core buffers?'
 };
+var colours = require('./colours.json');
 var shorthands = require('./shorthands.json');
-var shorthandRegExp = RegExp(Object.keys(shorthands).reduce(function (pv, cv, ci, a) {
+var shorthandRegExp = new RegExp(Object.keys(shorthands).reduce(function (pv, cv, ci, a) {
     var o = pv;
-    if (ci !== 0)
-        o += "\\b|\\b";
+    if (ci !== 0) {
+        o += '\\b|\\b';
+    }
     o += cv;
-    if(ci == a.length-1)
+    if(ci == a.length-1) {
         o+='\\b';
+    }
     return o;
 }, '\\b'),'i');
 
@@ -30,14 +33,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // POST request returns JSON
 app.post('/', function (req, res) {
-    if (!req.body) return res.sendStatus(400);
+    if (!req.body){
+        return res.sendStatus(400);
+    }
     var postData = req.body;
 
     sendResponse(postData.text, postData.token === postToken, function (o) {
-        if (o)
-            res.json({ text: o });
-        else
+        if (o) {
+            res.json(o);
+        }
+        else {
             res.sendStatus(500);
+        }
     }, postData.trigger_word);
 });
 
@@ -47,10 +54,21 @@ app.get('/', function (req, res) {
 
     sendResponse(getData.text, getData.token === getToken, function (o) {
         res.type('text/plain');
-        if (o)
-            res.send(o);
-        else
+         if (o) {
+            res.write(o.text);
+            if (o.attachments) {
+                if (o.attachments[0].title)
+                    res.write(o.attachments[0].title) + '\n';
+                if (o.attachments[0].pretext)
+                    res.write(o.attachments[0].pretext) + '\n';
+                if (o.attachments[0].text) {
+                    res.write('\n>' + o.attachments[0].text.split('\n').join('\n>'));
+                }
+            }
+            res.end();
+        } else {
             res.sendStatus(500);
+        }
     });
 });
 
@@ -63,24 +81,17 @@ function sendResponse (text, correctToken, responder, trigger) {
     }
 
     if (!correctToken) {
-        return responder(messages.INVALID_TOKEN);
+        return responder({'text': messages.INVALID_TOKEN});
     } else if (!text || !text.length) {
-        return responder(messages.NO_QUERY);
+        return responder({'text': messages.NO_QUERY});
     }
     
-    if (trigger && trigger == 'decklist:') {
-        return decklist(text, function (deck) {
-            responder(formatDeck(deck));
-        }, function () {
-            responder(messages.NO_RESULTS);
-        });
-    }
     search(text, function ($, panel) {
         responder(formatSingle($, panel));
-    }, function (matches) {
-        responder(formatMultiple(matches));
+    }, function ($, matches) {
+        responder(formatMultiple($, matches));
     }, function () {
-        responder(messages.NO_RESULTS);
+        responder({'text': messages.NO_RESULTS});
     }, function () {
         responder(null);
     });
@@ -91,41 +102,63 @@ function sendResponse (text, correctToken, responder, trigger) {
  *
  * @param   matches The array of cards matching the query, returned from NRDB
  */
-function formatMultiple (matches) {
-    var o = '';
-    matches = matches.text().split('\n').map(function (s) {
-        return clean(s);
-    }).filter(function (s) {
-        return s.length > 0;
-    });
-    o += matches.length;
-    if (matches.length > maxHits) {
-        o += messages.TOO_MANY;
-    } else {
-        o += messages.MULTIPLE_RESULTS + '\n\n';
-        matches.map(function (s) {
-            o += '  • ' + s + '\n';
-        });
+function formatMultiple ($, matches) {
+    var a = {'text':'', 'attachments':[{text:''}]};
+    var hits = matches.length;
+
+    if (hits > maxHits) {
+        return {'text': hits + messages.TO_MANY};
     }
-    return o;
+
+    a.text = hits + messages.MULTIPLE_RESULTS;
+    matches.each(function (i, e) {
+        e = $(e);
+        var text = clean(e.text());
+        var url = e.find('a').attr('href');
+        a.attachments[0].text += '• <' + url + '|' + text + '>\n'; 
+    });
+
+    return a;
+}
+
+function formatCardInfo(info, faction) {
+    info = info.replace(/^(.*?)( •|:)/, '*$1*$2');
+    info = info.replace(/ • /g,', ');
+    info += '\n' + faction;
+    if (!info.match(/(Agenda|Identity)/)) {
+        var influence = parseInt(info.replace(/.*Influence: (\d+)/, '$1'));
+        info = info.replace(/, Influence: \d+/, '');
+        for (var i = 0; i < influence; i++) {
+            info += '•';
+        }
+    }
+    info = info.replace(/Memory: (\d)/, ':_$1mu:');
+    info = info.replace(/Strength: (\d+)/, '$1 str');
+    info = info.replace(/(?:Rez|Install|Cost): (\d+)/, '$1:_credit:');
+    info = info.replace(/, Adv: (\d+), Score: (\d+)/, ' ($1/$2)');
+    info = info.replace(/Trash: (\d+)/, '$1:_trash:');
+    info = info.replace(/Link: (\d+)/, '$1:_link:');
+    return info;
 }
 
 /**
  * Converts a single card from NetrunnerDB into text to be returned to slack.
  */
 function formatSingle ($, panel) {
-    var o = '';
-    var flavor;
-    o += '*' + clean(panel.find('.panel-heading').text()).replace('♦', '◆') + '*\n';
-    o += clean(panel.find('.card-info').text()) + '\n';
+    var a = {'text':'', 'attachments':[{pretext:'', text:''}]};
+    var title = clean(panel.find('.panel-heading').text()).replace('♦', '◆');
+    var faction = clean(panel.find('.card-illustrator').text()).replace(/ •.*/g, '');
+    var info = clean(panel.find('.card-info').text());
+    a.text = '<' + panel.find('a.card-title').attr('href');
+    a.text +=  '|*' + title + '*>\n';
+    a.attachments[0].pretext = formatCardInfo(info, faction);
     panel.find('.card-text p').each(function (i, p) {
-        o += '> ' + clean($(p).text()) + '\n';
+        a.attachments[0].text += clean($(p).text()) + '\n';
     });
-    flavor = clean(panel.find('.card-flavor').text());
-    if (flavor.length) o += '_' + flavor + '_\n';
-    o += clean(panel.find('.card-illustrator').text()) + '\n';
-    o += panel.find('a.card-title').attr('href');
-    return o;
+    a.attachments[0].fallback = 'NRDB results for ' + title;
+    a.attachments[0].mrkdwn_in = ['pretext', 'text'];
+    a.attachments[0].color = colours[faction];
+    return a;
 }
 
 /**
@@ -147,10 +180,9 @@ function substitute (body) {
     body = body.replace(/<span class="icon icon-credit"><\/span>/g, ':_credit:');
     body = body.replace(/<span class="icon icon-trash"><\/span>/g, ':_trash:');
     body = body.replace(/<span class="icon icon-link"><\/span>/g, ':_link:');
+    body = body.replace(/([1-3])<span class="icon icon-mu"><\/span>/g, ':_$1mu:');
     body = body.replace(/<span class="icon icon-mu"><\/span>/g, ':_mu:');
-    body = body.replace(/<span class="icon icon-1mu"><\/span>/g, ':_1mu:');
-    body = body.replace(/<span class="icon icon-2mu"><\/span>/g, ':_2mu:');
-    body = body.replace(/<span class="icon icon-3mu"><\/span>/g, ':_3mu:');
+    body = body.replace(/<span class="icon icon-([1-3])mu"><\/span>/g, ':_$1mu:');
     body = body.replace(/<span class="icon icon-recurring-credit"><\/span>/g, ':_recurringcredit:');
     body = body.replace(/<span class="icon icon-subroutine"><\/span>/g, ':_subroutine:');
     body = body.replace(/<strong>/g, '*');
@@ -167,13 +199,13 @@ function substitute (body) {
  * @param Function oneResult Callback if one card is found
  * @param Function manyResults Callback if more than one card is found
  * @param Function noResults Callback if no cards are found
- * @param Function error Callback if there was an error
+ * @param Function errorResult Callback if there was an error
  */
-function search (text, oneResult, manyResults, noResults, error) {
+function search (text, oneResult, manyResults, noResults, errorResult) {
     oneResult = oneResult || _noop;
     manyResults = manyResults || _noop;
     noResults = noResults || _noop;
-    error = error || _noop;
+    errorResult = errorResult || _noop;
 
     // If forceful mode is on, pick any exact match when there are multiples
     var forceful = false;
@@ -188,62 +220,35 @@ function search (text, oneResult, manyResults, noResults, error) {
     }).toLowerCase();
 
     request('http://netrunnerdb.com/find/?q=' + text, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var $ = cheerio.load(substitute(body));
-            var panel = $('.panel');
-            var matches = $('[data-th="Title"]');
+        if (error || response.statusCode !== 200) {
+            return errorResult();
+        }
+        var $ = cheerio.load(substitute(body));
+        var panel = $('.panel');
+        var matches = $('[data-th="Title"]');
 
-            if (panel && panel.length === 1) {
-                oneResult($, panel);
-            } else if (matches.length) {
-                var found = false;
-                if (forceful) {
-                    matches.each(function (i, m) {
-                        m = $(m);
-                        var re = RegExp('^'+text, 'i');
-                        if (!found && clean(m.text()).match(re)) {
-                            found = true;
-                            request(m.find('a').attr('href'), function (error, response, body) {
-                                var $ = cheerio.load(substitute(body));
-                                oneResult($, $('.panel'));
-                            });
-                        }
-                    });
-                } else {
-                    manyResults(matches);
-                }
+        if (panel && panel.length === 1) {
+            oneResult($, panel);
+        } else if (matches.length) {
+            var found = false;
+            if (forceful) {
+                matches.each(function (i, m) {
+                    m = $(m);
+                    var re = new RegExp('^'+text, 'i');
+                    if (!found && clean(m.text()).match(re)) {
+                        found = true;
+                        request(m.find('a').attr('href'), function (error, response, body) {
+                            var $ = cheerio.load(substitute(body));
+                            oneResult($, $('.panel'));
+                        });
+                    }
+                });
             } else {
-                noResults();
+                manyResults($, matches);
             }
         } else {
-            error();
+            noResults();
         }
-    });
-}
-
-function formatDeck (deck) {
-    var o = '*' + deck.name + '*';
-    o += ' by ' + deck.username;
-    o += ' (' + deck.creation.replace(/(\d+)-(\d+)-(\d+).*/, "$3/$2/$1") + ')\n';
-    return o;
-}
-
-function decklist (text, result, noResults, error) {
-    result = result || _noop;
-    noResults = noResults || _noop;
-    error = error || _noop;
-
-    text = text.replace(/(.*netrunnerdb.com\/en\/decklist\/)?(\d+)(\/.*)?/i,"$2");
-
-    request('http://netrunnerdb.com/api/decklist/' + text, function (error, response, body) {
-        if (error || response.statusCode != 200) {
-            if (response.statusCode == 404)
-                return noResults();
-            return error();
-        }
-
-        var deck = JSON.parse(body);
-        result(deck);
     });
 }
 
